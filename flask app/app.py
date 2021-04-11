3f5eb968847c01e775715ff828537d2cf7817ed9
Kraczla, Maciej <maciej.kraczla@cgi.com>
2021-04-11 11:11:44 +0200

    first commit

---
+++ flask app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+
+USER = os.getenv('API_USER')
+SECRET = os.getenv('SECRET')
+
+
+@app.route('/')
+def index():
+    return "helllo {user}".format(user=USER)
+
+@app.route('/secret')
+def secret():
+    return "your secret {s}".format(s=SECRET)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
