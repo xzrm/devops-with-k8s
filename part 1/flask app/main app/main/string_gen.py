bf3fd9efc75172761771bb7c19dab13859a2c009
Maciej <maciekkra@gmail.com>
2021-04-13 18:12:43 +0200

    added volumes & communication between pods

--- part 1/flask app/main app/main/string_gen.py
+++ part 1/flask app/main app/main/string_gen.py
@@ -3,9 +3,15 @@ import os
 import random
 import string
 import datetime
+import requests
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 
+path = os.path.join('/', 'usr', 'src' ,'app', 'files')
+if not os.path.exists(path):
+    os.makedirs(path)
+
+file_path = os.path.join(path, 'count.txt')
 
 app = Flask(__name__)
 
@@ -20,20 +26,38 @@ def generate_random_string():
         timestamp = datetime.datetime.utcnow()
         result_str = ''.join(random.choice(letters) for i in range(10))
         log = "{}: {}".format(timestamp, result_str)
-        print(log)
         logs.append(log)
         sleep(5)
 
+def read_file(path):
+    try:
+        with open(path, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "File not found"
 
 
 @app.route('/')
 def index():
+    # txt = read_file(file_path)
+    response = requests.get('http://pong-app-service:8000/count')
+    count = response.json()
     try:
-        return logs[-1]
+        return "<p>{}</p>  <p>Ping / Pong: {}</p>".format(logs[-1], count['count'])
     except IndexError as error:
         return "no logs available"
+
+@app.route('/files')
+def get_files():
+    files = os.listdir(path)
+    return ''.join(files)
 
 
 if __name__ == '__main__':
     executor.submit(generate_random_string)
+    print("starting")
     app.run(debug=True, host='0.0.0.0', use_reloader=False)
+
+
+
+
