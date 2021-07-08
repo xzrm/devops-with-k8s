400b19e419fd19f6624164f3b8f475f0493e6c3c
Maciej <maciekkra@gmail.com>
2021-07-08 20:22:10 +0200

    removed nats

--- project/broadcaster/config.py
+++ project/broadcaster/config.py
@@ -13,4 +13,4 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_SIZE = 100
-    RABBITMQ_URI = os.getenv("RABBITMQ_URI")
+    NATS_URI = os.getenv("NATS_URI")
--- project/docker_build&push-frontend.sh
+++ project/docker_build&push-frontend.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-IMAGE="flask-app"
-cd ./backend-app
+IMAGE="frontend-app"
+cd ./frontend-app
 docker build  -t "xzarem/${IMAGE}:${IMAGE}" .
 docker push "xzarem/${IMAGE}:${IMAGE}"
 SHA256=$(docker inspect --format='{{index .RepoDigests 0}}' "xzarem/${IMAGE}:${IMAGE}" | sed 's/^.*sha256:\(.*\)$/\1/')
 echo "$SHA256"
 cd ..
 cd ./manifest
-sed "s/imageTag/${SHA256}/g" flask-app_template.yaml > flask-app.yaml
+sed "s/imageTag/${SHA256}/g" react-app_template.yaml > react-app.yaml
 
 
 
